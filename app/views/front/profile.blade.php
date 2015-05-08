0ff6a3f39e1115cd59b2ed76d19fff162f920f1c
quocanhfa <quocanhfa@gmail.com>
2015-05-08 14:13:48 +0700

    Delete sample code

--- app/views/front/profile.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('front.layout')
-
-@section('content')
-  <h2>Welcome "{{ Auth::user()->username }}" to the protected page!</h2>
-  <p>Your user ID is: {{ Auth::user()->id }}</p>
-  {{ Auth::user()->birthday }}
-@stop
