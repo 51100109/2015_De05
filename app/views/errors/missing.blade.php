afb00ce67636a9a2ec7c41b5001a7d49a97a622b
quocanhfa <quocanhfa@gmail.com>
2015-05-10 13:46:55 +0700

    Add 404 page

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,13 @@
+@extends('front.layouts.mainlayout')
+
+@section('title')
+404 - Softsharing
+@endsection
+
+@section('content')
+ <p class="pull-right visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
+          </p>
+       	<h2>404 Not Found</h2>
+       	<div>Trang bạn tìm hiện không có</div>
+@endsection
